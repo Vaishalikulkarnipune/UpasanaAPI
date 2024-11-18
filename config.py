a4f19470dc5817b2fa813abc9aee0ce601dccee8
shashankpawan <shashank.pawan@gmail.com>
2024-11-18 17:45:24 +0530

    Adding DB details which got removed in earlier commit

--- config.py
+++ config.py
@@ -15,19 +15,23 @@ logging.basicConfig(
         logging.StreamHandler()
     ]
 )
-
 class Config:
-    # Load encryption key and encrypted password
+    # Load the encryption key and encrypted password from environment variables
     cipher = Fernet(os.getenv("ENCRYPTION_KEY").encode())
     ENCRYPTED_PASSWORD = os.getenv("ENCRYPTED_PASSWORD").encode()
-
+    
     # Decrypt the password
     DB_PASSWORD = cipher.decrypt(ENCRYPTED_PASSWORD).decode()
+    
+    # Use the connection pool URI for upasanadbpool
+    SQLALCHEMY_DATABASE_URI = f"postgresql://doadmin:{DB_PASSWORD}@db-postgresql-blr1-14444-do-user-18154576-0.i.db.ondigitalocean.com:25060/defaultdb?sslmode=require&application_name=upasanadbpool"
 
-    # Connection pool URI
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Connection Pool Configuration (use pool URI directly)
     CONNECTION_POOL_URI = f"postgresql://doadmin:{DB_PASSWORD}@db-postgresql-blr1-14444-do-user-18154576-0.i.db.ondigitalocean.com:25060/defaultdb?sslmode=require&application_name=upasanadbpool"
 
-    # Connection pool configuration
+    # Create connection pool using the connection pool URI
     connection_pool = pool.SimpleConnectionPool(
         1, 22,  # Adjust maxconn as needed
         dsn=CONNECTION_POOL_URI
